acf5cfa1bb9dd5908a677307b21da11b280c3677
João Santos <joao75559@gmail.com>
2025-08-20 07:30:28 -0300

    startServer func added

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,8 +4,18 @@ import { connectDB } from './config/db.js';
 
 const app = express();
 
-connectDB();
-
 app.get("/", (req,res) => res.send("Welcome to the backend server!"));
 
-app.listen(ENV.PORT, () => console.log('Server is running on port ', ENV.PORT));
+
+const startServer = async () => {
+    try {
+        await connectDB();
+
+        app.listen(ENV.PORT, () => console.log('Server is running on port:', ENV.PORT));
+    } catch (error) {
+        console.error('Failed to start server:', error);
+        process.exit(1);
+    }
+};
+
+startServer();
