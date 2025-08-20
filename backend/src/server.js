69ca3665735f683ac32ec25188e8e85b9eb639c3
João Santos <joao75559@gmail.com>
2025-08-20 18:32:35 -0300

    jk

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,30 @@
 import express from 'express';
-
+import { ENV } from './config/env.js';
+import { connectDB } from './config/db.js';
+import cors from 'cors';
+import { clerkMiddleware } from '@clerk/express';
+import userRoutes from './routes/user.route.js';
 
 const app = express();
 
-app.listen(5001, () => { 
-    console.log('Server is running on port 5001'); 
-});
+app.use(cors())
+app.use(express.json())
+
+app.use(clerkMiddleware());
+
+app.get("/", (req,res) => res.send("Welcome to the backend server!"));
+
+app.use("/api/users", userRoutes);
+
+const startServer = async () => {
+    try {
+        await connectDB();
+
+        app.listen(ENV.PORT, () => console.log('Server is running on port:', ENV.PORT));
+    } catch (error) {
+        console.error('Failed to start server:', error);
+        process.exit(1);
+    }
+};
+
+startServer();
